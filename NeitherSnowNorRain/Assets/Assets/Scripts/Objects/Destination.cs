f575bb19d810e5360dd1b9521f9459ea8e31821c
Gee Eff <fl3mbobs@gmail.com>
2014-10-18 16:03:47 +0100

    Added destination + collisions

---
+++ NeitherSnowNorRain/Assets/Assets/Scripts/Objects/Destination.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destination : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+			EventManager.SendPlayerDestinationEvent ();
+		}
+		
+		if(coll.gameObject.tag == "Delivery"){
+			EventManager.SendDeliveryDestinationEvent(this,coll);
+		}
+		
+		Destroy (this.gameObject);
+	}
+}
